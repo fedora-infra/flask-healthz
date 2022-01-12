4022f3284f687029ec7c0b75bc48279c4f67e293
Aurélien Bompard <aurelien@bompard.org>
2022-01-12 14:38:18 +0100

    Update the get-changes script
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- get-changes.sh
+++ get-changes.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-last_tag=`git tag | sort -n | tail -n 1`
+last_tag="$1"
+if [ -z "$last_tag" ]; then
+    last_tag=`git tag | sort -n | tail -n 1`
+fi
 git log ${last_tag}.. --no-merges --invert-grep  --author dependabot --format="* %s (%h)"

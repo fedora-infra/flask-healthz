bfcada53a2e74094122a0b7c4ae26b33b064c39c
Lenka Segura <lsegura@redhat.com>
2023-08-21 13:48:06 +0200

    Replace dependabot with renovate
    Signed-off-by: Lenka Segura <lsegura@redhat.com>

--- get-changes.sh
+++ get-changes.sh
@@ -6,4 +6,4 @@ last_tag="$1"
 if [ -z "$last_tag" ]; then
     last_tag=`git tag | sort -n | tail -n 1`
 fi
-git log ${last_tag}.. --no-merges --invert-grep  --author dependabot --format="* %s (%h)"
+git log ${last_tag}.. --no-merges --invert-grep  --author renovate --format="* %s (%h)"
